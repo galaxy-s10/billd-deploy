109306d3bffb3da77a82ad16da451cfece33113f
shuisheng <2274751790@qq.com>
2024-02-20 16:55:02 +0800

    feat: log

--- src/utils/chalkTip.ts
+++ src/utils/chalkTip.ts
@@ -1,10 +1,20 @@
 import chalk from 'chalk';
 
 export const chalkINFO = (v) =>
-  `${chalk.bgBlueBright.black(' INFO ')} ${chalk.blueBright(v)}`;
+  `${chalk.bgBlueBright.black(
+    `[${new Date().toLocaleString()}]  INFO   `
+  )} ${chalk.blueBright(v)}`;
 export const chalkSUCCESS = (v) =>
-  `${chalk.bgGreenBright.black(' SUCCESS ')} ${chalk.greenBright(v)}`;
+  `${chalk.bgGreenBright.black(
+    `[${new Date().toLocaleString()}] SUCCESS `
+  )} ${chalk.greenBright(v)}`;
 export const chalkERROR = (v) =>
-  `${chalk.bgRedBright.black(' ERROR ')}  ${chalk.redBright(v)}`;
+  `${chalk.bgRedBright.black(
+    `[${new Date().toLocaleString()}]  ERROR  `
+  )} ${chalk.redBright(v)}`;
 export const chalkWARN = (v) =>
-  `${chalk.bgHex('#FFA500').black(' WARN ')} ${chalk.hex('#FFA500')(v)}`;
+  `${chalk
+    .bgHex('#FFA500')
+    .black(`[${new Date().toLocaleString()}]  WARN   `)} ${chalk.hex('#FFA500')(
+    v
+  )}`;
